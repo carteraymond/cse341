46f21c9fa73cdbaf76ddfb9f47ee136d938e5586
carteraymond <craymond78@gmail.com>
2024-09-28 12:55:27 -0400

    fixed GET fo single id

--- Controller/contacts.js
+++ Controller/contacts.js
@@ -13,7 +13,7 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
+    .db('contacts')
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
