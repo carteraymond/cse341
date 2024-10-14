0195a2f78ac00e74dfd57bb7ad7920db90a8c103
carteraymond <craymond78@gmail.com>
2024-10-13 21:58:04 -0400

    allowed CRUD Operations

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
   })
   .use('/', require('./Route'));
